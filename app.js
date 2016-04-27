4ee2d192e4e21371e379f7169d2aa67b7724ba51
Marco Genova <mgenova03@gmail.com>
2016-04-27 14:27:17 -0400

    Added files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var main = function() {
+  $('form').submit(function() {
+    var firstName = $('#first').val();
+    if(firstName === "") {
+     	$('.first-name-error').text("Please enter your first name");
+    }
+    var lastName = $('#last').val();
+    if(lastName === "") {
+     	$('.last-name-error').text("Please enter your last name");
+    }
+    var email = $('#email').val();
+    if(email === "") {
+     	$('.email-error').text("Please enter your email address");
+    } 
+    var password = $('#password').val();
+    if(password === "") {
+     	$('.password-error').text("Please enter a password");
+    } else if(password.length < 8) {
+      $('.password-error').text("Short passwords are easy to guess. Try one with at least 8 characters.");
+    }
+
+    return false;
+  });
+}
+
+$(document).ready(main);
